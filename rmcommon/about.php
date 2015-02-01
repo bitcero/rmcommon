f6e668d997f01b079bd0febf99142bfbb898041b
Eduardo Cortes <i.bitcero@gmail.com>
2015-02-01 14:43:58 -0600

    Updated docs and about

--- rmcommon/about.php
+++ rmcommon/about.php
@@ -34,8 +34,8 @@ RMTemplate::get()->header(); ?>
     <div class="col-xs-12">
         <h1 class="page-header">
             <img src="<?php echo RMCURL; ?>/images/cu-gear.png" class="pull-left" style="width: 80px; margin-right: 10px;">
-            Bienvenido a Common Utilities <?php echo RMCVERSION; ?><br>
-            <small>Gracias por instalar Common Utilities <?php echo RMCVERSION; ?>. Esta versión tiene muchas novedades para tí.</small>
+            Welcome to Common Utilities<br>
+            <small>Thanks for using Common Utilities <?php echo RMCVERSION; ?>. This version has a lot of improvements for you.</small>
         </h1>
         <hr>
     </div>
@@ -45,14 +45,14 @@ RMTemplate::get()->header(); ?>
     <div class="col-sm-4">
         <div class="cu-box">
             <div class="box-content">
-                <h2>Aspecto visual mejorado</h2>
-                <img src="//www.xoopsmexico.net/about/rmcommon/schemes/all.jpg" class="img-responsive " alt="Aspecto mejorado">
-                <p>El tema por defecto de Common Utilities (Two·P6) ha sido mejorado y le hemos otorgado un aspecto
-                    mas moderno y limpio.</p>
+                <h2>Improved GUI</h2>
+                <img src="//www.xoopsmexico.net/about/rmcommon/schemes/all.jpg" class="img-responsive" style="margin-bottom: 10px;" alt="Improved GUI">
+                <p>The default theme of Common Utilities (Two·P6) has been improved and we have gived to it a better, modern and clean
+                    appearance.</p>
                 <hr>
-                <h3 class="text-info">Elije una Combinación</h3>
-                <p class="help-block">Seis nuevos esquemas de colores han sido incluidos en esta versión de Common Utilities, junto con la posibilidad de crear
-                    tus propias combinaciones.</p>
+                <h3 class="text-info">Choose a Color Scheme</h3>
+                <p class="help-block">Six new color schemes has been included in this version of Two·Six, and you can
+                    <a href="./?twop6=about">create your own schemes</a>.</p>
                 <div class="row">
                     <div class="col-xs-4  col-sm-12 col-md-6 text-center">
                         <a href="#" class="twop6-scheme thumbnail" data-file="theme-default.css" style="background-image: url('//www.xoopsmexico.net/about/rmcommon/schemes/default.png');" title="Default Color Sheme" data-rel="tooltip">
@@ -96,6 +96,9 @@ RMTemplate::get()->header(); ?>
                         <strong>Voy-Boy</strong>
                     </div>
                 </div>
+
+                <hr>
+
             </div>
         </div>
 
@@ -103,47 +106,70 @@ RMTemplate::get()->header(); ?>
 
     <div class="col-sm-8">
 
-        <h2 class="text-success text-center">Módulos mejores y más modernos</h2>
+        <h2 class="text-success text-center">Better and modern modules</h2>
         <p class="lead text-center">
-            Common Utilities ofrece las herramientas necesarias para crear mejores módulos con características modernas. Su soporte MVC
-            hace la creación de módulos más sencilla que nunca.
+            Common Utilities provide the neccessary tools to make better modules with modern features.
+            As if this were not enough, it's integrated MVC support allows you to create modules faster and easily as ever.
         </p>
         <hr>
 
-        <h3>Nuevos Componentes</h3>
+        <h3>New Components</h3>
         <div class="row">
 
             <div class="col-sm-4">
-                <h4 class="text-info">Gestor de Módulos</h4>
+                <h4 class="text-info">Modules Manager</h4>
                 <p class="help-block">
-                    El <strong>Gestor de Módulos</strong> integrado en Common Utilities permite una administración más efectiva
-                    de los módulos, tanto nativos de XOOPS como de CU.
+                    The new <strong>Modules Manager</strong> of Common Utilities allows a more effective control of
+                    modules.
                 </p>
             </div>
             <div class="col-sm-4">
-                <h4 class="text-info">Gestor de Bloques</h4>
+                <h4 class="text-info">Blocks Manager</h4>
                 <p class="help-block">
-                    El nuevo gestor de bloques te permite toda la libertada que hasta ahora no tenías. Crea posiciones, copias del mismo
-                    bloque y adminístralos como nunca lo habías hecho.
+                    The Common Utilities block manager provide you a full freedom to manage your blocks: create new
+                    blocks positions, add blocks and a super comfortable interface.
                 </p>
             </div>
             <div class="col-sm-4">
-                <h4 class="text-info">Gestor de Imágenes</h4>
+                <h4 class="text-info">Images Manager</h4>
                 <p class="help-block">
-                    Cargar y administrar imágenes nunca ha sido tan fácil. Crea categorías para organizarlas y asigna diferentes tamaños;
-                    Common Utilities se encarga de todo lo demás.
+                    Upload and manage images never has been more easy.
+                    Organize them by categories and assign different sizes for each one. Common Utilities make the hard work
+                    for you.
                 </p>
             </div>
 
         </div>
         <hr>
 
-        <h2 class="text-primary">Configuración mas Poderosa</h2>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <h3 class="text-center" style="margin-bottom: 20px;">Markdown editor and full screen edition</h3>
+                <div class="row">
+                    <div class="col-sm-5">
+                        <a href="settings.php"><img src="//www.xoopsmexico.net/about/rmcommon/md.png" alt="Nuevo editor Markdown" class="img-responsive img-thumbnail"></a>
+                    </div>
+                    <div class="col-sm-7">
+                        <p>Common Utilities now includes a <strong>Markdown editor</strong> (Github Flavored), with highlight syntax,
+                            available to use in any module or component.</p>
+                        <hr>
+                        <p>
+                            Additionally, all editors (Visual, HTMl, Simple, XoopsCode and Markdown) now includes a great feature:
+                            <strong>Full screen edition</strong>. Work without distractions and be more productive.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+
+        <h2 class="text-primary text-center">More Powerful Configuration</h2><br>
         <div class="row">
             <div class="col-sm-6">
                 <p>El nuevo sistema de confgiuración de Common Utilities (<em>tambien disponible para módulos nativos de XOOPS</em>), permite
-                una configuración mas poderosa de los módulos al proporcionar nuevos tipos de campos y la modificación de los
-                valores en línea.</p>
+                    una configuración mas poderosa de los módulos al proporcionar nuevos tipos de campos y la modificación de los
+                    valores en línea.</p>
                 <p>
                     Con el soporte para categorías de opciones de configuración y un diseño mucho mas limpio, ajustar el comportamiento
                     de los módulos para adaptarlos a tus necesidades es mucho mas fácil.
@@ -158,15 +184,19 @@ RMTemplate::get()->header(); ?>
 
         <div class="row">
             <div class="col-md-6">
-                <h3>Actualizaciones inmediatas</h3>
-                <img src="//www.xoopsmexico.net/about/rmcommon/updates.png" alt="Actualizaciones automáticas" class="img-responsive img-thumbnail">
-                <p class="help-block">
-                    ¡Se acabaron los sufrimientos para mantener actualizado el sistema! Con el sistema de actualizaciones de
-                    Common Utilities, las actualizaciones están <strong class="text-success">a un solo click de distancia</strong>.
-                </p>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h3>1-Click Updates</h3>
+                        <img src="//www.xoopsmexico.net/about/rmcommon/updates.png" alt="Actualizaciones automáticas" class="img-responsive img-thumbnail">
+                        <p class="help-block">
+                            ¡Se acabaron los sufrimientos para mantener actualizado el sistema! Con el sistema de actualizaciones de
+                            Common Utilities, las actualizaciones están <strong class="text-success">a un solo click de distancia</strong>.
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-md-6">
-                <h3>Seguridad mejorada</h3>
+                <h3>Improved Permissions</h3>
                 <img src="//www.xoopsmexico.net/about/rmcommon/security.png" alt="Seguridad mejorada" class="img-responsive img-thumbnail">
                 <p class="help-block">
                     Módulos con un mejor control sobre cada una de sus características mediante el nuevo sistema de seguridad
