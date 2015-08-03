828a06e05aa88acd86f964de640091ddec6ddca4
Eduardo Cortes <i.bitcero@gmail.com>
2015-08-03 16:53:37 -0500

    Updating version number

--- rmcommon/loader.php
+++ rmcommon/loader.php
@@ -17,7 +17,7 @@ if ( !defined( 'RMCURL' ) )
     define("RMCURL",XOOPS_URL.'/modules/rmcommon');
 define('ABSURL', XOOPS_URL);
 define('ABSPATH', XOOPS_ROOT_PATH);
-define('RMCVERSION','2.2.8.8');
+define('RMCVERSION','2.2.9.6');
 
 /**
  * Welcome Screen
