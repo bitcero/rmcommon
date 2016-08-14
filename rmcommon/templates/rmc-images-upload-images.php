7b4c4c5a9f399f92052edb4b40071737e2b856b1
Eduardo Cortes <i.bitcero@gmail.com>
2016-08-13 21:52:44 -0500

    Fixed #83

--- rmcommon/templates/rmc-images-upload-images.php
+++ rmcommon/templates/rmc-images-upload-images.php
@@ -1,4 +1,4 @@
-<?php if(!$isupdate): ?>
+<?php if(false == $isupdate): ?>
 <h1 class="cu-section-title"><?php _e('Upload Images','rmcommon'); ?></h1>
 <div class="select-image-category">
 	<form name="frmcat" method="get" action="images.php" class="form-inline">
@@ -14,36 +14,53 @@
 	<input type="hidden" name="action" value="new" />
 	</form>
 </div>
-<?php if (!$cat->isNew()): ?>
-
-<div id="upload-controls">
-    <div id="upload-errors">
-
-    </div>
-</div>
-
-        <div id="files-container">
-            <form class="dropzone" id="images-uploader">
-                <input type="hidden" name="category" value="<?php echo $cat->id(); ?>">
-            </form>
-        </div>
-
-        <div id="images-resizing">
-            <h4><?php _e('Resizing images', 'rmcommon'); ?></h4>
-            <div class="progress">
-                <div id="bar-indicator" class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0;">
-
-                </div>
-            </div>
-            <span class="message"></span>
-        </div>
-
-        <div id="uploading-messages">
-            <h4><?php _e('Uploading messages', 'rmcommon'); ?></h4>
-            <ul>
-            </ul>
-        </div>
-
 <?php endif; ?>
+
+<?php if (false == $cat->isNew()): ?>
+
+    <?php if($showControls): ?>
+    <div id="upload-controls">
+        <div id="upload-errors">
+
+        </div>
+    </div>
+
+    <div id="files-container">
+        <form class="dropzone" id="images-uploader">
+            <input type="hidden" name="category" value="<?php echo $cat->id(); ?>">
+        </form>
+    </div>
+    <?php endif; ?>
+
+    <div id="images-resizing">
+        <h4><?php _e('Resizing images', 'rmcommon'); ?></h4>
+        <div class="progress">
+            <div id="bar-indicator" class="progress-bar progress-bar-info progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0;">
+
+            </div>
+        </div>
+        <span class="message"></span>
+    </div>
+
+    <?php if($isupdate): ?>
+
+        <div id="back-images" class="text-center" style="margin: 15px 0; display: none;">
+            <a href="images.php?category=<?php echo $cat->id(); ?>&amp;page=<?php echo $page; ?>" class="btn btn-success"><?php _e('Back to images', 'rmcommon'); ?></a>
+        </div>
+
+    <?php endif; ?>
+
+    <div id="uploading-messages">
+        <h4>
+            <?php if(isset($messagesTitle)): ?>
+                <?php echo $messagesTitle; ?>
+            <?php else: ?>
+                <?php _e('Uploading messages', 'rmcommon'); ?>
+            <?php endif; ?>
+        </h4>
+        <ul>
+        </ul>
+    </div>
+
 <?php endif; ?>
 
