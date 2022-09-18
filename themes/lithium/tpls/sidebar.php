7bfeade06e3b9d639cfc2ba18e201243519751c7
bitcero <i.bitcero@gmail.com>
2022-09-17 19:04:45 -0500

    Add new styles for sidebar

--- themes/lithium/tpls/sidebar.php
+++ themes/lithium/tpls/sidebar.php
@@ -8,33 +8,41 @@
             </div>
             <div class="sidebar-toggle">
                 <a href="#" class="btn btn-link" id="toggle-sidebar">
-                    <i class="fa fa-bars"></i>
+                    <span class="visually-hidden">Toggle sidebar</span>
                 </a>
             </div>
         </div>
     </div>
 
-    <div class="module-search">
-        <input type="text" value="" class="form-control" id="filter-module" placeholder="<?php _e('Search modules...', 'rmcommon'); ?>">
-    </div>
-
     <div class="menu-wrapper">
+        <div class="module-search">
+            <input type="text" value="" class="form-control" id="filter-module" placeholder="<?php _e('Search modules...', 'lithium'); ?>">
+        </div>
+
         <div class="sidebar-menu">
 
             <!-- CURRENT MODULE MENU -->
             <?php if (isset($currentModule) && !empty($currentModule)): ?>
                 <h4 class="menu-heading current-module-head">
-                    <a href="#menu-<?php echo $currentModule->directory; ?>"><?php echo $currentModule->name; ?></a>
+                    <a href="#menu-<?php echo $currentModule->directory; ?>" class="text-truncate"><?php echo $currentModule->name; ?></a>
                 </h4>
                 <?php if (!empty($currentModule->menu)): ?>
                 <ul id="menu-<?php echo $currentModule->directory; ?>" class="current-module-menu">
                     <?php foreach ($currentModule->menu as $menu): ?>
                         <li<?php echo $menu['location'] == $common->location ? ' class="active"' : ''; ?>>
-                            <a <?php echo isset($menu['attributes']) ? $xoFunc->render_attributes($menu['attributes']) : ''; ?> href="<?php echo $xoFunc->menuLink((object) $menu, $currentModule); ?>"<?php echo $menu['location'] == $common->location ? ' class="open"' : ''; ?> <?php if (array_key_exists('options', $menu) && !empty($menu['options'])): ?> data-submenu="yes"<?php endif; ?>>
-                                <?php echo $xoFunc->menuIcon($menu['icon'], $currentModule->directory); ?>
-                                <?php echo $menu['title']; ?>
+                            <a
+                                <?php echo isset($menu['attributes']) ? $xoFunc->render_attributes($menu['attributes']) : ''; ?>
+                                href="<?php echo $xoFunc->menuLink((object) $menu, $currentModule); ?>"
+                                class="d-flex align-items-center justify-content-between <?php echo $menu['location'] == $common->location ? 'open' : ''; ?>"
                                 <?php if (array_key_exists('options', $menu) && !empty($menu['options'])): ?>
-                                    <span class="caret"></span>
+                                data-submenu="yes"<?php endif; ?>
+                            >
+                                <span class="d-flex align-items-center justify-content-start">
+                                    <?php echo $xoFunc->menuIcon($menu['icon'], $currentModule->directory); ?>
+                                    <span class="menu-title text-truncate"><?php echo $menu['title']; ?></span>
+                                </span>
+                                <?php if (array_key_exists('options', $menu) && !empty($menu['options'])): ?>
+                                  <?php echo $common->icons()->getIcon('svg-lithium-angle-right', [], false); ?>
                                 <?php endif; ?>
                             </a>
                             <?php if (array_key_exists('options', $menu) && !empty($menu['options'])): ?>
@@ -46,9 +54,13 @@
                                         <li>
                                             <a
                                                 href="<?php echo $xoFunc->menuLink((object) $submenu, $currentModule); ?>"
-                                                <?php echo isset($submenu['attributes']) ? $xoFunc->render_attributes($submenu['attributes']) : ''; ?>>
+                                                <?php echo isset($submenu['attributes']) ? $xoFunc->render_attributes($submenu['attributes']) : ''; ?>
+                                                class="d-flex align-items-center justify-content-between"
+                                            >
+                                              <span class="d-flex align-items-center justify-content-start">
                                                 <?php echo isset($submenu['icon']) ? $xoFunc->menuIcon($submenu['icon'], $currentModule->directory) : ''; ?>
-                                                <?php echo $submenu['title']; ?>
+                                                  <span class="menu-title text-truncate"><?php echo $submenu['title']; ?></span>
+                                              </span>
                                             </a>
                                         </li>
                                     <?php endforeach; ?>
@@ -62,17 +74,24 @@
 
             <!-- INSTALLED MODULES MENU -->
             <h4 class="menu-heading modules-menu-head">
-                <a href="#menu-modules"><?php _e('Modules', 'rmcommon'); ?></a>
+                <a href="#menu-modules" class="text-truncate"><?php _e('Modules', 'rmcommon'); ?></a>
             </h4>
             <?php if (!empty($activeModules)): ?>
                 <ul id="menu-modules">
                     <?php foreach ($activeModules as $module): ?>
                         <li>
-                            <a href="<?php echo RMUris::anchor($module->directory); ?>"<?php if (!empty($module->menu)): ?> data-submenu="yes"<?php endif; ?>>
-                                <?php echo $xoFunc->menuIcon($module->icon, $module->directory); ?>
-                                <?php echo $module->name; ?>
+                            <a
+                                href="<?php echo RMUris::anchor($module->directory); ?>"
                                 <?php if (!empty($module->menu)): ?>
-                                    <span class="caret"></span>
+                                data-submenu="yes"<?php endif; ?>
+                                class="d-flex align-items-center justify-content-between"
+                            >
+                                <span class="d-flex align-items-center justify-content-start">
+                                    <?php echo $xoFunc->menuIcon($module->icon, $module->directory); ?>
+                                    <span class="menu-title text-truncate"><?php echo $module->name; ?></span>
+                                </span>
+                                <?php if (!empty($module->menu)): ?>
+                                  <?php echo $common->icons()->getIcon('svg-lithium-angle-right', [], false); ?>
                                 <?php endif; ?>
                             </a>
                             <?php if (!empty($module->menu)): ?>
